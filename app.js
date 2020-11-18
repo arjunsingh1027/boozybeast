d4db912e534d674f1d82623c6897ca102caf33e0
klharmany <klharmany@gmail.com>
2020-11-18 11:39:20 -0700

    changes to app with lines 53-55 and index with lines 19-26

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$("#rando").on("click", function () {
+$(".movie-btn").on("click", function () {
   var page = $(this).attr("movie-page");
   var random = Math.floor((Math.random(page) * 10) + 1);
   var genreId = parseInt($("#genres").val());
@@ -11,7 +11,8 @@ $("#rando").on("click", function () {
     method: "GET",
   }).then(function (response) {
     console.log(response);
-    $('#movies-here').empty();
+    $('.movie-info-wrapper').empty();
+    $('#movieP').empty();
     for (var i = 0; i < 1; i++) {
       var imgURL =
         "https://image.tmdb.org/t/p/w200/" + response.results[i].poster_path;
@@ -19,38 +20,56 @@ $("#rando").on("click", function () {
       const $title = $("<h1>").text("Title: " + response.results[i].title);
       const $plot = $("<h3>").text("Plot: " + response.results[i].overview);
       const $poster = $("<img>").attr("src", imgURL);
-      $moviediv.append($title, $poster, $plot);
-      $("#movies-here").prepend($moviediv);
+      $moviediv.append($title, $plot);
+      $(".movie-info-wrapper").prepend($moviediv);
+      $("#movieP").prepend($poster);
     }
   });
 });
 
-$("#clear-btn").on("click", function () {
-  $("#movies-here").empty();
-});
+// $("#clear-btn").on("click", function () {
+//   $("#movies-here").empty();
+// });
 $(".btn-wrapper").on("click", function (e) {
   e.preventDefault();
 });
-$("#drink").on("click", function () {
+
+// $(".base").on("click",function() { 
+//   var baseingredient = $("#baseingredient").val();
+//   var queryURL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${baseingredient}`
+   
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET",
+//   }).then(function (response) {
+//     console.log(response);
+
+
+
+
+$(".drink-btn").on("click", function () {
   // var page = $(this).attr("movie-page");
   // var random = Math.floor((Math.random(page) * 10) + 1);
+  var baseingredient = $("#baseingredient").val();
   var queryURL =
-    "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+  `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${baseingredient}`;
   $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
     console.log(response);
-    $('#movies-here').empty();
+    $('.drink-info-wrapper').empty();
+    $('#drinkImg').empty();
     for (var i = 0; i < 1; i++) {
       // var imgURL =
       //   "https://image.tmdb.org/t/p/w200/" + response.results[i].poster_path;
       const $drinkdiv = $("<div>");
       const $name = $("<h2>").text("Name: " + response.drinks[i].strDrink);
-      const $alch = $("<h2>").text(response.drinks[i].strAlcoholic);
+      // const $alch = $("<h2>").text(response.drinks[i].strAlcoholic);
       const $drink = $("<img>").attr("src", response.drinks[i].strDrinkThumb);
-      $drinkdiv.append($name, $alch, $drink);
-      $("#movies-here").prepend($drinkdiv);
+      $drinkdiv.append($name);
+      $(".drink-info-wrapper").prepend($drinkdiv);
+      $("#drinkImg").prepend($drink);
     }
   });
 });
---
+++ drinkdropdown.js
@@ -0,0 +1,83 @@
+// //Lookup cocktail name by spirits 
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+
+
+// $(".base").on("click",function() { 
+//   var baseingredient = $("#baseingredient").val();
+//   var queryURL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${baseingredient}`
+   
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET",
+//   }).then(function (response) {
+//     console.log(response);
+    // $('.movie-info-wrapper').empty();
+    // $('#movieP').empty();
+
+    // for (var i=0;i<1;i++) {
+
+    // }
+  
+  
+  // $.ajax({
+  //   url: queryURL, 
+  //   method:"GET",
+  //   success: function(response)
+  //    { console.log(response)
+  //    }
+  })
+  })
+
+  // getdrink(gin)
+
+  // .then(function(response) {
+  // // return response.json ()
+  // console.log("Success!")}
+  // ) 
+
+  
+
+  
+  // //called ajax for link above
+  // $.ajax({
+  //   url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?i=",
+  //   method: "Submit"
+  // }).then(function(1) {
+  //   console.log(vodka);
+  // });
+
+  // $.ajax({
+  //   url: "https://www.thecocktaildb.com/api/json/v1/2/search.php?i=",
+  //   method: "Submit"
+  // }).then(function(2) {
+  //   console.log(whiskey);
+  // });
+
+  // $.ajax({
+  //   url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?i=rum",
+  //   method: "GET"
+  // }).then(function(response) {
+  //   console.log(response);
+  // });
+
+  // $.ajax({
+  //   url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?i=gin",
+  //   method: "GET"
+  // }).then(function(response) {
+  //   console.log(response);
+  // });
+
+  // $.ajax({
+  //   url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?i=brandy",
+  //   method: "GET"
+  // }).then(function(response) {
+  //   console.log(response);
+  // });
+
+  // $.ajax({
+  //   url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?i=tequila",
+  //   method: "GET"
+  // }).then(function(response) {
+  //   console.log(response);
+  // });
